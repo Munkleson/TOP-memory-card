5ec5bc9d330293466161af78c8c8ddb8253b4910
Munkleson <alexwongsydney@gmail.com>
2024-09-04 12:30:33 +0900

    Further fixed issue with algorithm that didn't set the number of pokemon per row correctly when it came to mobiles

--- src/gameSettingsVariables.js
+++ src/gameSettingsVariables.js
@@ -6,7 +6,7 @@ const gameSettings = {
     mobileOrNot: (window.innerWidth / window.innerHeight) < 1, //// To check if div separators are needed because mobile. Essentially a mobile check
 
 };
-gameSettings.maxPokemonPerRow = gameSettings.mobileOrNot ? Math.floor(gameSettings.maxNumberOfPokemon / 5) : Math.floor(gameSettings.maxNumberOfPokemon / 3); //// Changes when to separate the cards for evenness depending on if it's a mobile device or not
+gameSettings.maxPokemonPerRow = gameSettings.mobileOrNot ? Math.floor(gameSettings.maxNumberOfPokemon / 6) : Math.floor(gameSettings.maxNumberOfPokemon / 3); //// Changes when to separate the cards for evenness depending on if it's a mobile device or not
 
 export { gameSettings };
 
