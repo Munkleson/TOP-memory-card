801a2e7c219aafb947f79e887bf73c88fd62f85e
Munkleson <alexwongsydney@gmail.com>
2024-09-07 20:37:32 +0900

    Nothing makes sense, so trying to set the instruction data another way and see if it works when I set the data when the component mounts rather than having the data set upon initialization of app

--- src/components/GameModeData.js
+++ src/components/GameModeData.js
@@ -11,30 +11,4 @@ const gameModeData = {
     }
 }
 
-gameModeData.classic.instructions = [
-    `• In this game mode, you will have a number of cards per round based on the difficulty level. All of them will be shown at the same time`,
-    `• Once a card is ${gameSettings.mobileOrNot ? "tapped" : "clicked"}, the deck will shuffle and the cards may be displayed in a previous position than they were previously`,
-    `• The number of cards per round will depend on the difficulty level. The settings are as follows:`,
-    `    - ${gameModeData.classic.difficulties[0]}: 6 cards`,
-    `    - ${gameModeData.classic.difficulties[1]}: 12 cards`,
-    `    - ${gameModeData.classic.difficulties[2]}: 20 cards`,
-    `    - ${gameModeData.classic.difficulties[3]}: 30 cards`,
-    `    - ${gameModeData.classic.difficulties[4]}: You may choose a number from 6 to 30 cards`,
-    ``,
-    ``,
-];
-
-gameModeData.standard.instructions = [
-    "• In this game mode, you will have a number of cards per round based on the difficulty level. However, only a number of them will be displayed at a time",
-    `• Once a card is ${gameSettings.mobileOrNot ? "tapped" : "clicked"}, the deck will shuffle and show you a random selection of cards out of all the cards in the deck`,
-    "• The number of cards displayed will depend on the difficulty level. The settings are as follows:",
-    `    - ${gameModeData.standard.difficulties[0]}: 3 cards displayed out of 6`,
-    `    - ${gameModeData.standard.difficulties[1]}: 6 cards displayed out of 12`,   
-    `    - ${gameModeData.standard.difficulties[2]}: 9 cards displayed out of 20`, 
-    `    - ${gameModeData.standard.difficulties[3]}: 9 cards displayed out of 30`, 
-    `    - ${gameModeData.standard.difficulties[4]}: 9 cards displayed out of 99`, 
-    `    - ${gameModeData.standard.difficulties[5]}: Up to a maximum of 9 cards displayed. You may choose a number from 6 to 99 cards`,          
-    "* There will always be at least one valid card displayed",
-];
-
 export { gameModeData };
--- src/components/ModeInstructions.jsx
+++ src/components/ModeInstructions.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import styles from "./ModeInstructions.module.css";
 import { gameModeData } from "./GameModeData";
+import { gameSettings } from "../gameSettingsVariables";
 
 export default function ModeInstructions({ openAndCloseInstructions, openAndCloseMenu }){
+    setInstructionData();
     const [selectedMode, setSelectedMode] = useState("");
-    console.log(selectedMode)
     function selectMode(event){
         setSelectedMode(event.target.innerText.toLowerCase());
-        console.log(event.target.innerText.toLowerCase())
     }
     return (
         <>
@@ -49,10 +49,6 @@ function DisplayModes({ element, selectMode, selectedMode, }){
 }
 
 function DisplayInstructions({ selectedMode }){
-    console.log(selectedMode);
-    console.log(gameModeData);
-    console.log(gameModeData[selectedMode]);
-    console.log(gameModeData[selectedMode].instructions)
     return (
         <>              
             {gameModeData[selectedMode].instructions.map((element, index) => {
@@ -64,5 +60,30 @@ function DisplayInstructions({ selectedMode }){
     )
 }
 
-
-
+function setInstructionData(){
+    gameModeData.classic.instructions = [
+        `• In this game mode, you will have a number of cards per round based on the difficulty level. All of them will be shown at the same time`,
+        `• Once a card is ${gameSettings.mobileOrNot ? "tapped" : "clicked"}, the deck will shuffle and the cards may be displayed in a previous position than they were previously`,
+        `• The number of cards per round will depend on the difficulty level. The settings are as follows:`,
+        `    - ${gameModeData.classic.difficulties[0]}: 6 cards`,
+        `    - ${gameModeData.classic.difficulties[1]}: 12 cards`,
+        `    - ${gameModeData.classic.difficulties[2]}: 20 cards`,
+        `    - ${gameModeData.classic.difficulties[3]}: 30 cards`,
+        `    - ${gameModeData.classic.difficulties[4]}: You may choose a number from 6 to 30 cards`,
+        ``,
+        ``,
+    ];
+    
+    gameModeData.standard.instructions = [
+        "• In this game mode, you will have a number of cards per round based on the difficulty level. However, only a number of them will be displayed at a time",
+        `• Once a card is ${gameSettings.mobileOrNot ? "tapped" : "clicked"}, the deck will shuffle and show you a random selection of cards out of all the cards in the deck`,
+        "• The number of cards displayed will depend on the difficulty level. The settings are as follows:",
+        `    - ${gameModeData.standard.difficulties[0]}: 3 cards displayed out of 6`,
+        `    - ${gameModeData.standard.difficulties[1]}: 6 cards displayed out of 12`,   
+        `    - ${gameModeData.standard.difficulties[2]}: 9 cards displayed out of 20`, 
+        `    - ${gameModeData.standard.difficulties[3]}: 9 cards displayed out of 30`, 
+        `    - ${gameModeData.standard.difficulties[4]}: 9 cards displayed out of 99`, 
+        `    - ${gameModeData.standard.difficulties[5]}: Up to a maximum of 9 cards displayed. You may choose a number from 6 to 99 cards`,          
+        "* There will always be at least one valid card displayed",
+    ];
+}
